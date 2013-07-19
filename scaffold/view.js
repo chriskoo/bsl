88152f7bc2e3972f3b4b29aeabf6b392818a23bc
Juno Lueng <Juno@JunotekiMacBook-Pro.local>
2013-07-19 22:24:41 +0800

    modify view
    modify view

--- scaffold/view.js
+++ scaffold/view.js
@@ -1,16 +1,32 @@
 define(['require', 'cube/cube', 'text!<%= module %>/<%= view %>.html'],
-	function(require, Cube, viewTemplate){
+	function(require, Cube, viewTemplate) {
 
-	return Cube.View.extend({
+		return Cube.View.extend({
 
-		id: '<%= module %>_<%= view %>',
+			id: '<%= module %>_<%= view %>',
 
-		render: function(){
-			$(this.el).html(viewTemplate);
+			initialize: function() {
 
-			Cube.View.prototype.render.call(this);
-			return this;
-		}
-	});//view define
+			},
 
-});
+			events: {
+
+			},
+
+			bindings: {
+
+			},
+
+			render: function() {
+				$(this.el).html(viewTemplate);
+
+				Cube.View.prototype.render.call(this);
+				return this;
+			},
+
+			onShow: function() {
+
+			}
+		}); //view define
+
+	});
